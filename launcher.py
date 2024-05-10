8936f11004fb78f267d1ac0247b9f37ce937fbfb
MNeMoNiCuZ <60541708+MNeMoNiCuZ@users.noreply.github.com>
2024-05-10 14:20:45 +0200

    Delete launcher.py

--- launcher.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-import subprocess
-
-# Launch the Flask app in a new terminal
-subprocess.Popen(["python", "app.py"])
-
-# Open the web browser to the Flask application
-webbrowser.open("http://localhost:5000/", new=2)
